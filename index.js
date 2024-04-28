0beffcf290ff170d446d70b9a57a37b4b6bf01a4
Anindo <anindo.ah@gmail.com>
2024-04-29 01:12:11 +0600

    update get method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require ('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,6 +27,19 @@ async function run() {
     await client.connect();
 
     const craftCollection = client.db('textileDB').collection('art&craft')
+
+    app.get('/crafts', async(req, res) => {
+        const cursor = craftCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/crafts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await craftCollection.findOne(query);
+      res.send(result)
+    })
 
     app.post('/crafts', async(req, res) => {
         const newCraft = req.body;
